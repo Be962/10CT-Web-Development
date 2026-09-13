20c2fcc1babf7043a9fee0f5d46acea367bd2d23
Obviousslasher13 <obviousslasher@gmail.com>
2026-09-13 22:44:31 +1000

    Added videos, folders, functionality etc.
    This is a long one. Today I cut up the videos, exported them and encoded them via ffmpeg to make sure they run well. Then i organised them into each page, 7 in total. Added folders for JS, CSS, and HTML. Each HTML page has its own CSS stylesheet as well as the main one, allowing for more organised text allocation.

---
+++ JS/scrubbing.js
@@ -0,0 +1,25 @@
+const section = document.querySelector('section.vid') // Select the section with the video
+const vid = section.querySelector('video') // Select the video within the section
+
+vid.pause()
+
+const scroll = () => {
+    const distance = window.scrollY - section.offsetTop // Distance that has been scrolled minus the distance to get to the top of the section
+    const total = section.clientHeight - window.innerHeight // Distance to get to bottom of section, minus the height of the window because scroll counts from top of the window    
+
+    let percentage = distance / total // Percentage of video to be played
+
+    // Make sure that percentage is always between 0-1
+    percentage = Math.max(0, percentage) // Pick the larger one between percentage and 0
+    percentage = Math.min(percentage, 1) // Pick the smaller one between percentage and 1
+
+    if (vid.duration > 0) { // Video loaded
+        vid.currentTime = vid.duration * percentage // Set the time of the video to the amount that has been scrolled 
+    }
+}
+
+
+scroll()
+window.addEventListener("scroll", scroll) // Request how far has been scrolled
+
+
